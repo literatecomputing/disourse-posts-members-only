3778272b01f6390c30e88e2587d79fff454fe45a
Jay Pfaffman <pfaffman@gmail.com>
2022-07-12 14:19:56 -0500

    update setting name and package.json

--- plugin.rb
+++ plugin.rb
@@ -1,13 +1,33 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
-# about: TODO
-# version: 0.0.1
-# authors: Discourse
-# url: TODO
+# name: discourse-posts-members-only
+# about: Show posts only to logged in users
+# version: 0.0.2
+# authors: Jay Pfaffman <ay@literatecomputing.com>
+# url: https://github.com/literatecomputing/disourse-posts-members-only
 # required_version: 2.7.0
 
-enabled_site_setting :plugin_name_enabled
+enabled_site_setting :posts_members_only_enabled
 
 after_initialize do
+  class ::TopicsController
+    module MyTopicShow
+      def show
+        ensure_logged_in
+        super
+      end
+    end
+    prepend MyTopicShow
+  end
+
+  class ::SearchController
+    module MyTopicShow
+      def show
+        ensure_logged_in
+        super
+      end
+    end
+    prepend MyTopicShow
+  end
+
 end
